afaeba1b19c15e36bf1aa58db24a3d3f7230d006
David Triendl <david@triendl.name>
2008-07-21 17:12:59 +0200

    Retabbed include/wikipage.php
    Use universal 4 whitespace to indent stuff. Nothing to see here, please
    move on.

--- include/wikipage.php
+++ include/wikipage.php
@@ -8,70 +8,71 @@ class WikiPage
 {
     function __construct($path, $commit=NULL)
     {
-	global $repo;
-
-	$this->path = $path;
-
-	if ($commit === NULL)
-	    $commit = $repo->getObject($repo->getHead(Config::GIT_BRANCH));
-	try
-	{
-	    $this->object = WikiPage::find_page($commit, $path);
-	}
-	catch (InvalidPageError $e)
-	{
-	    $this->object = NULL;
-	}
+        global $repo;
+        $this->path = $path;
+        if ($commit === NULL)
+            $commit = $repo->getObject($repo->getHead(Config::GIT_BRANCH));
+        try
+        {
+            $this->object = WikiPage::find_page($commit, $path);
+        }
+        catch (InvalidPageError $e)
+        {
+            $this->object = NULL;
+        }
     }
+
     function get_url()
     {
-	$url = Config::PATH;
-	foreach ($this->path as $part)
-	    $url .= '/' . strtr(str_replace('_', '%5F', urlencode($part)), '+', '_');
-	return $url;
+        $url = Config::PATH;
+        foreach ($this->path as $part)
+            $url .= '/' . strtr(str_replace('_', '%5F', urlencode($part)), '+', '_');
+        return $url;
     }
+
     function get_name()
     {
-	return implode('/', $this->path);
+        return implode('/', $this->path);
     }
+
     function format()
     {
-	return Markup::markup2html($this->object->data);
+        return Markup::markup2html($this->object->data);
     }
 
     static function from_url($name, $commit=NULL)
     {
-	$path = array();
-	$dir = FALSE;
-	foreach (explode('/', $name) as $part)
-	{
-	    $dir = FALSE;
-	    if (!empty($part))
-		array_push($path, urldecode(strtr($part, '_', ' ')));
-	    else
-		$dir = TRUE;
-	}
-	if (count($path) == 0)
-	    $path = array('Home');
-	else if ($dir)
-	    array_push($path, '');
-	return new WikiPage($path, $commit);
+        $path = array();
+        $dir = FALSE;
+        foreach (explode('/', $name) as $part)
+        {
+            $dir = FALSE;
+            if (!empty($part))
+                array_push($path, urldecode(strtr($part, '_', ' ')));
+            else
+                $dir = TRUE;
+        }
+        if (count($path) == 0)
+            $path = array('Home');
+        else if ($dir)
+            array_push($path, '');
+        return new WikiPage($path, $commit);
     }
+
     static function find_page($commit, $path)
     {
-	$cur = $commit->repo->getObject($commit->tree);
-	while (count($path))
-	{
-	    if ($cur->getType() != Git::OBJ_TREE)
-		throw new InvalidTreeError;
-	    if ($path[0] == '')
-		break;
-	    if (!isset($cur->nodes[$path[0]]))
-		throw new PageNotFoundError;
-	    $cur = $commit->repo->getObject($cur->nodes[array_shift($path)]->object);
-	}
-	return $cur;
+        $cur = $commit->repo->getObject($commit->tree);
+        while (count($path))
+        {
+            if ($cur->getType() != Git::OBJ_TREE)
+                throw new InvalidTreeError;
+            if ($path[0] == '')
+                break;
+            if (!isset($cur->nodes[$path[0]]))
+                throw new PageNotFoundError;
+            $cur = $commit->repo->getObject($cur->nodes[array_shift($path)]->object);
+        }
+        return $cur;
     }
 }
 
-?>
