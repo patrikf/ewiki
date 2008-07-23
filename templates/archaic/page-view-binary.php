32ab9c4c0058fa794be674b21fd0426111770994
Patrik Fimml <patrik@fimml.at>
2008-07-23 20:40:42 +0200

    Refer to current commit in download link

--- templates/archaic/page-view-binary.php
+++ templates/archaic/page-view-binary.php
@@ -1,4 +1,4 @@
 <?php include('header.php'); ?>
 <h1 id="pagetitle"><?= Markup::escape($page->get_name()) ?></h1>
-<p><a href="<?= $page->get_url() ?>?action=get">Download “<?= Markup::escape($page->get_name()) ?>”</a></p>
+<p><a href="<?= $page->get_url() ?>?action=get&amp;commit=<?= $commit_id ?>">Download “<?= Markup::escape($page->get_name()) ?>”</a></p>
 <?php include('footer.php');
