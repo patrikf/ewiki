ef6afbfc6290b3dbf343cdf15fd38495f8bf160f
Patrik Fimml <patrik@fimml.at>
2008-07-24 17:29:54 +0200

    Beautify page-view-image.php, fix image spacing

--- templates/archaic/page-view-image.php
+++ templates/archaic/page-view-image.php
@@ -1,5 +1,14 @@
 <?php include('header.php'); ?>
 <h1 id="pagetitle"><?= Markup::escape($page->getName()) ?></h1>
-<a href="<?= $page->getURL() ?>?action=get&amp;commit=<?= $commit_id ?>"><img src="<?= $page->getURL() ?>?action=image&amp;width=<?= Config::IMAGE_WIDTH ?>&amp;height=<?= Config::IMAGE_HEIGHT ?>" alt="<?= Markup::escape($page->getName()) ?>" /></a>
-<p><a href="<?= $page->getURL() ?>?action=get&amp;commit=<?= $commit_id ?>">View full size: “<?= Markup::escape($page->getName()) ?>”</a></p>
+<p>
+    <a href="<?= $page->getURL() ?>?action=get&amp;commit=<?= $commit_id ?>">
+        <img
+            src="<?= $page->getURL() ?>?action=image&amp;width=<?= Config::IMAGE_WIDTH ?>&amp;height=<?= Config::IMAGE_HEIGHT ?>"
+            alt="<?= Markup::escape($page->getName()) ?>" />
+    </a>
+</p><p>
+    <a href="<?= $page->getURL() ?>?action=get&amp;commit=<?= $commit_id ?>">
+        View full size: “<?= Markup::escape($page->getName()) ?>”
+    </a>
+</p>
 <?php include('footer.php');
