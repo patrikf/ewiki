7df37b8d6419a9a36d0e1c8d22ee984396fabad2
Patrik Fimml <patrik@fimml.at>
2008-07-27 19:34:50 +0200

    Add history warning

--- templates/archaic/page-view.php
+++ templates/archaic/page-view.php
@@ -1,6 +1,14 @@
 <? $title = $page->getName(); ?>
 <? require('header.php'); ?>
 <h1 id="pagetitle"><?= Markup::escape($page->getName()) ?></h1>
+<? if (!$commit_is_tip): ?>
+<div class="history-warning">
+You are currently viewing an old version (commit
+<span class="sha1"><?= $commit_id ?></span>) of this page. The
+<a href="<?= $page->getURL() ?>">current version</a> might show
+significant differences.
+</div>
+<? endif; ?>
 <?php
 
 $map = array(
